17c79b75e6504ceb635b8e8bdaedec5116882eaf
David <david@A224.local>
2025-12-26 13:05:13 +0100

    dark status and navigation bar -> light icons

--- app/src/main/java/com/davidcrespo/onewallet/MainActivity.kt
+++ app/src/main/java/com/davidcrespo/onewallet/MainActivity.kt
@@ -2,6 +2,7 @@ package com.davidcrespo.onewallet
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
@@ -11,13 +12,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.davidcrespo.onewallet.presentation.designsystem.theme.OneWalletTheme
 import com.davidcrespo.onewallet.presentation.navigation.MainNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb()),
+            navigationBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb())
+        )
         setContent {
             OneWalletTheme {
                 Box(
